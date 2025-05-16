cb090a94b88297ad8d4d7de37c19a9bf5bd02e08
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-16 21:33:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+function submit(){
+
+	const title=document.getElementById('title').value.trim();
+	const author=document.getElementById('author').value.trim();
+	const isbn=document.getElementById('isbn').value.trim();
+	if(title==='' || author==='' || isbn===''){
+		alert("Please fill in all fields");
+		return
+	}
+
+	const booklist=document.getElementById('book-list').value
+	const row=document.createElement('tr')
+
+	row.innerHTML=`
+	<td>${title}</td>
+	<td>${author}</td>
+	<td>${isbn}</td>
+	<td><button class="delete">X</button></td>
+	`
+	booklist.appendChild(row)
+
+	document.getElementById("title").value=""
+	document.getElementById("author").value=""
+	document.getElementById("isbn").value=""
+}
