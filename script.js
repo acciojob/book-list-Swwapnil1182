5f5688da3240d58975e043d2b809e931a60c99d6
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-16 21:40:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,4 +23,10 @@ function submit(){
 	document.getElementById("title").value=""
 	document.getElementById("author").value=""
 	document.getElementById("isbn").value=""
+
+	document.getElementById('book-list').addEventListener('click', function (e) {
+			if (e.target.classList.contains('delete')) {
+				e.target.parentElement.parentElement.remove();
+			}
+		});
 }
